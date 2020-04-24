6d7bdcf3b158046d561458b9cbe62934076d78fa
Nikhil Sharma <atnikhil96@gmail.com>
2020-04-24 13:14:50 +0530

    rxjs part 2

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -37,4 +37,8 @@ export class DishService {
         setTimeout(()=>resolve(DISHES.filter((dish)=>(dish.featured))[0]),2000)
       });*/
   }
+
+  getDishIds(): Observable<string | any>{
+      return of(DISHES.map(dish=>dish.id));
+  }
 }
