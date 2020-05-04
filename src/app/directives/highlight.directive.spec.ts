7d14a6fbe447cdc67b5d5df72493bf912c5d7674
Nikhil Sharma <atnikhil96@gmail.com>
2020-05-04 17:19:03 +0530

    Component Test

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,17 @@
 import { HighlightDirective } from './highlight.directive';
 
 describe('HighlightDirective', () => {
+  let elRefMock = {
+    nativeElement: document.createElement('div')
+  };
+  
+  // let rendm={
+  //   rendere:addClass() 
+  // }
+  const rendererMock = jasmine.createSpyObj('rendererMock', ['addClass','removeClass']); 
+
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
+    const directive = new HighlightDirective(elRefMock, rendererMock);
     expect(directive).toBeTruthy();
   });
 });
